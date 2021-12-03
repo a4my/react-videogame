9b70b64aa3d213514640a6254b25061f0f7d9efb
Alex Fourmy <alexfourmy@gmail.com>
2021-12-03 08:54:06 +0100

    Worked on the homepage layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import Cards from '../components/Cards.jsx'
 
 export default function Home() {
-  return <div></div>
+  return (
+    <div className="home">
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+    </div>
+  )
 }
