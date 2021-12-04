153ebbb7eaaac09c407664204912f128a3c22ad6
Alex Fourmy <alexfourmy@gmail.com>
2021-12-04 18:59:30 +0100

    Finished styling the sidebar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="navbar">
+        <div className="logo">
+          <p>GAMER</p>
+          <i className="fas fa-gamepad"></i>
+        </div>
+        <div className="searchbar">
+          <i className="fas fa-search"></i>
+          <input type="text" placeholder="Search for a game" />
+        </div>
+        <div className="account">
+          <i className="fas fa-bell"></i>
+          <i className="fas fa-plus"></i>
+          <i className="fas fa-cog"></i>
+          <p>AF</p>
+        </div>
+      </div>
+    </>
+  )
+}
