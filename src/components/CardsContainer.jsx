4aaab8eea0424ee1bc57b36a80ef649ce0be825a
Alex Fourmy <alexfourmy@gmail.com>
2021-12-04 18:58:53 +0100

    Created a new component

---
+++ src/components/CardsContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Cards from './Cards'
+
+export default function CardsContainer() {
+  return (
+    <div className="cardsContainer">
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+      <Cards />
+    </div>
+  )
+}
