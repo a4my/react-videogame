c41f09dba0d027310d9181a8b5d4a0de4b38f286
Alex Fourmy <alexfourmy@gmail.com>
2021-12-05 11:31:18 +0100

    Fetch data from an API

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import Cards from './Cards'
 
 export default function CardsContainer() {
+  const [data, setData] = useState([])
+  const [loadOnce, setLoadOnce] = useState(true)
+  const apiKey = process.env.REACT_APP_API_KEY
+
+  useEffect(() => {
+    if (loadOnce) {
+      axios.get('https://api.rawg.io/api/games?key=' + apiKey).then(res => {
+        setData(res.data.results)
+        setLoadOnce(false)
+      })
+    }
+  }, [apiKey, data, loadOnce])
+
   return (
     <div className="cardsContainer">
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
+      {data.map(game => (
+        <Cards game={game} key={game.id} />
+      ))}
     </div>
   )
 }
